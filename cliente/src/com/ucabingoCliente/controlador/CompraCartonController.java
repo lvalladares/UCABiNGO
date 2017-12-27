16c6a6307246541a4bc5f5bb1cecbda73cede923
Unknown <ismaelteixeira6@gmail.com>
2017-12-27 16:56:08 -0400

    Modulo de compra de carton terminado

--- cliente/src/com/ucabingoCliente/controlador/CompraCartonController.java
+++ cliente/src/com/ucabingoCliente/controlador/CompraCartonController.java
@@ -1,9 +1,78 @@
 package com.ucabingoCliente.controlador;
 
-public class RegistroController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
 
-	public RegistroController() {
-		// TODO Auto-generated constructor stub
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import com.ucabingoCliente.vista.CompraCarton;
+
+import com.google.gson.*;
+
+public class CompraCartonController implements ActionListener, ChangeListener{
+
+	private CompraCarton vista;
+	
+	public CompraCartonController(CompraCarton vista) {
+		this.vista = vista;
+		this.vista.frame.setVisible(true);
+		
+		//Asignacion de precios
+		this.vista.labelPrecio.setText("500");
+		this.vista.labelSaldo.setText("1500");
+		
+		//Asignacion del precio total de compra
+		this.vista.labelTotal.setText(String.valueOf(
+				(Integer.parseInt(this.vista.labelPrecio.getText()) * 
+				Integer.parseInt((String) this.vista.spinner.getValue())
+				)
+		));
+	
+		this.vista.spinner.addChangeListener(this);
+		this.vista.btnRegresar.addActionListener(this);
+		this.vista.btnComprar.addActionListener(this);
+	}
+	
+	//Accion que actualiza el precio total segun cantidad de cartones a comprar
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		this.vista.labelTotal.setText(String.valueOf(
+				(Integer.parseInt(this.vista.labelPrecio.getText()) * 
+				Integer.parseInt((String) this.vista.spinner.getValue())
+				)
+		));
+		
+		if (Integer.parseInt(this.vista.labelTotal.getText()) > Integer.parseInt(this.vista.labelSaldo.getText())) {
+			this.vista.btnComprar.setEnabled(false);
+			this.vista.labelMsjError.setText("Error. Saldo insuficiente.");
+		}
+		else {
+			this.vista.btnComprar.setEnabled(true);
+			this.vista.labelMsjError.setText("");
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//Accion de boton regresar
+		/*if (this.vista.btnRegresar == e.getSource()) {
+			
+			this.vista.frame.dispose();
+		}*/
+				
+		//Accion de boton comprar
+		if (this.vista.btnComprar == e.getSource()) {
+			String datos = "{'saldo':" + this.vista.labelSaldo.getText() + "',"
+						  + "'precioUnidad':"+ this.vista.labelPrecio.getText() + "',"
+						  + "'cantidad':"+ this.vista.spinner.getValue() + "',"
+						  + "'total':" + this.vista.labelTotal.getText() + "'}";
+			
+			final Gson gson = new Gson();
+		    final Properties json = gson.fromJson(datos, Properties.class);
+		    System.out.println(json);
+		}
 	}
 
 }
--- cliente/src/com/ucabingoCliente/main/Main.java
+++ cliente/src/com/ucabingoCliente/main/Main.java
@@ -1,10 +1,14 @@
 package com.ucabingoCliente.main;
 
+import com.ucabingoCliente.controlador.CompraCartonController;
+import com.ucabingoCliente.vista.CompraCarton;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// pruebas
+		CompraCarton vista = new CompraCarton();
+		CompraCartonController controlador = new CompraCartonController(vista);
 	}
 
 }
