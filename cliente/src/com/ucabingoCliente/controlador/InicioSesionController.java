ecb7b880ed9050ca5b646c1374a4c9911c1bd01a
Unknown <antoberar@gmail.com>
2017-12-28 01:37:57 -0400

    Ventana Ingresar Usuario Agregada

--- cliente/src/com/ucabingoCliente/controlador/InicioSesionController.java
+++ cliente/src/com/ucabingoCliente/controlador/InicioSesionController.java
@@ -1,9 +1,35 @@
 package com.ucabingoCliente.controlador;
 
-public class InicioSesionController {
+import com.ucabingoCliente.vista.InicioSesion;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-	public InicioSesionController() {
+/*Recordatorio; se debe agregar ActionListener para que los botones se entiendan
+como escuchadores.*/
+public class InicioSesionController implements ActionListener {
+	
+	private InicioSesion ventana;
+
+	public InicioSesionController(InicioSesion ventana) {
 		// TODO Auto-generated constructor stub
+		this.ventana = ventana;
+		this.ventana.frame.setVisible(true); 
+		this.ventana.botonGuardar.addActionListener(this);
+		this.ventana.botonCancelar.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent accion) {
+		if (this.ventana.botonGuardar == accion.getSource()) {
+			if ((this.ventana.nombreUsuario.getText().equals(""))) {
+				System.out.printf(" Se guardó de manera correcta");
+			}
+		}
+		if (this.ventana.botonCancelar == accion.getSource()) {
+			System.out.printf(" Ocurrio un error, revisar y volver a intentar");
+		}
 	}
 
 }
--- cliente/src/com/ucabingoCliente/vista/InicioSesion.java
+++ cliente/src/com/ucabingoCliente/vista/InicioSesion.java
@@ -3,10 +3,19 @@ package com.ucabingoCliente.vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class InicioSesion {
 
-	private JFrame frame;
+	public JFrame frame;
+	public JTextField nombreUsuario;
+	public JPasswordField passUsuario;
+	public JButton botonGuardar;
+	public JButton botonCancelar;
+	private JLabel labelUsuario;
 
 	/**
 	 * Launch the application.
@@ -37,7 +46,41 @@ public class InicioSesion {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("Inicio de Sesión");
+		frame.getContentPane().setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel labelUsuario = new JLabel("Ingrese nombre de Usuario");
+		labelUsuario.setBounds(20, 20, 200, 25);
+		frame.getContentPane().add(labelUsuario);
+		
+		nombreUsuario = new JTextField();
+		nombreUsuario.setBounds(225, 20, 200, 25);
+		frame.getContentPane().add(nombreUsuario);
+		
+		JLabel labelPass = new JLabel("Ingrese Contraseña");
+		labelPass.setBounds(35, 60, 200, 25);
+		frame.getContentPane().add(labelPass);
+		
+		passUsuario = new JPasswordField();
+		passUsuario.setBounds(225, 60, 200, 25);
+		frame.getContentPane().add(passUsuario);
+		
+		JLabel labelRepetirPass = new JLabel("Repetir Contraseña");
+		labelRepetirPass.setBounds(35, 100, 200, 25);
+		frame.getContentPane().add(labelRepetirPass);
+		
+		JPasswordField passUsuario2 = new JPasswordField();
+		passUsuario2.setBounds(225, 100, 200, 25);
+		frame.getContentPane().add(passUsuario2);
+		
+		botonGuardar = new JButton("Guardar");
+		botonGuardar.setBounds(50,225,100,40);
+		frame.getContentPane().add(botonGuardar);
+		
+		botonCancelar = new JButton("Cancelar");
+		botonCancelar.setBounds(300, 225, 100, 40);
+		frame.getContentPane().add(botonCancelar);
 	}
 
 }
