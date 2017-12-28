01c7747fb7a271271404fcfce31e7519967d08e3
Unknown <antoberar@gmail.com>
2017-12-28 19:37:29 -0400

    Ventana Menu Manejo
    Se agregó movilidad a la ventana menú a la ventana comprar cartón.

--- cliente/src/com/ucabingoCliente/controlador/MenuController.java
+++ cliente/src/com/ucabingoCliente/controlador/MenuController.java
@@ -1,25 +1,40 @@
 package com.ucabingoCliente.controlador;
 
 import com.ucabingoCliente.vista.Menu;
+import com.ucabingoCliente.vista.CompraCarton;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-
+/**
+ * @author antonio
+ *
+ */
 public class MenuController implements ActionListener{
 	private Menu ventana;
 	
 	public MenuController(Menu ventana) {
 		this.ventana = ventana;
 		this.ventana.frame.setVisible(true);
+		//Recordatorio: se debe agregar el actionListener para que funcione los botones.
+		this.ventana.inicioSesion.addActionListener(this);
+		this.ventana.comprarCarton.addActionListener(this);
+		this.ventana.cerrar.addActionListener(this);
 	}
 
 	public void actionPerformed(ActionEvent accion) {
 		// TODO Auto-generated method stub
+		if(this.ventana.inicioSesion == accion.getSource()) {
+			System.out.print("Voy a ingresar al sistema\n");
+		}
+		if (this.ventana.comprarCarton == accion.getSource()) {
+			CompraCarton ventanaNueva = new CompraCarton();
+			CompraCartonController controladorNuevo = new CompraCartonController(ventanaNueva);
+			this.ventana.frame.dispose();
+			
+		}
 		if (this.ventana.cerrar == accion.getSource()) {
 			this.ventana.frame.setVisible(false);
-			this.ventana.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.ventana.frame.dispose();
 		}
 	}
 }
--- cliente/src/com/ucabingoCliente/vista/RegistrarUsuario.java
+++ cliente/src/com/ucabingoCliente/vista/RegistrarUsuario.java
@@ -2,7 +2,6 @@ package com.ucabingoCliente.vista;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
